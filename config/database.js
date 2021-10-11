2be665bca6729b423a7550c3972fcd0b97c1d4bd
Adam Green <adam.green@kcl.ac.uk>
2021-10-12 00:08:35 +0100

    added exhaust .env vars

--- config/database.js
+++ config/database.js
@@ -1,6 +1,3 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   defaultConnection: env('DB_CONNECTION', 'default'),
   connections: {
@@ -8,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', '27017'),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: {
           rejectUnauthorized: false,
         },
