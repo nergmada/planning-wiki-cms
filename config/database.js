cd29add5d0ce5c1d7caca48f106c58a43cc880fd
Adam Green <adam.green@kcl.ac.uk>
2021-10-11 23:51:17 +0100

    Added two new environment variables

--- config/database.js
+++ config/database.js
@@ -1,6 +1,24 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env('DB_CONNECTION', 'default'),
   connections: {
+    postgres: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {},
+    },
     default: {
       connector: 'bookshelf',
       settings: {
