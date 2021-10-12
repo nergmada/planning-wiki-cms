abd118f1e0a4ef0d06843ff0dc784bac6f7d335b
Adam Green <adam.green@kcl.ac.uk>
2021-10-12 10:51:56 +0100

    retrieving DB_URL env var

--- config/database.js
+++ config/database.js
@@ -1,33 +1,37 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
-  defaultConnection: env('DB_CONNECTION', 'default'),
-  connections: {
-    postgres: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+  const config = parse(env('DATABASE_URL'));
+  console.log(env('DATABASE_URL'));
+  return ({
+    defaultConnection: env('DB_CONNECTION', 'default'),
+    connections: {
+      postgres: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        options: {},
+      },
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
         },
       },
-      options: {},
     },
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+  })
+
+};
